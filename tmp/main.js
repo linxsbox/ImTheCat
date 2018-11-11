4fed1bb401d9ae2d3441c6e2e70e16736df91c6a
Linxs <yunfax@outlook.com>
2018-11-11 23:30:34 +0800

    删除多余以及测试使用的资源和代码文件
    - 删除自带的png图片资源
    - 删除测试使用的临时项目文件

--- tmp/main.js
+++
@@ -1,9 +0,0 @@
-import estm from '../tmp/testModule.ES'
-estm.whatsThis() // 猜猜this输出是啥？
-
-const tm = require('../tmp/testModule.Common')
-tm.whatsThis() // 猜猜this输出是啥？
-
-const console = require('console')
-
-console.log(13)
