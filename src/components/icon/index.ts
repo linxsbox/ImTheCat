f834d48148431d273019db224acf4fc471832b13
shaw-555 <shaw18664590570@163.com>
2020-12-14 20:05:49 +0800

    feat: 初步开发icon

---
+++ src/components/icon/index.ts
@@ -0,0 +1,5 @@
+// import * as apis from '@/apis/asidebar';
+
+function getFn () { return ['asidebar']; }
+
+export default getFn;
---
+++ src/components/icon/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span class="cat-icon- cat-icon-home" ></span>
+  </div>
+</template>
+
+<script lang="ts">
+// https://github.com/kaorun343/vue-property-decorator
+// https://github.com/vuejs/vue-class-component\
+import { defineComponent, Ref, ref, onMounted, h } from '@vue/composition-api';
+import { Getters } from 'cat-vuex-decorator';
+// import * as h from './index';
+import { computed } from '@vue/composition-api';
+const prefix = 'cat-icon';
+
+export default defineComponent({
+
+
+  // data
+  name:'icon',
+
+
+  setup () {
+    computed(()=>{
+      return prefix;
+    });
+    return {
+      name: ref('icon')
+    };
+  }
+
+});
+
+</script>
+
+<style lang="scss" socpe src="./index.scss"></style>
